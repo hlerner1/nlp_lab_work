aa07a5d7d48cb74dda912dae8d9a41cafb26af96
Hannah Lerner <hannahlerner1@gmail.com>
2020-02-27 18:43:18 -0500

    added summary and characters to dataset script
    will upload physical dataset later. Will take a while to run.

--- goodreads/good_reads_scraper.py
+++ goodreads/good_reads_scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import time
 
 print("test")
 books = []
@@ -12,28 +13,66 @@ all_links = []
 pages = content.find_all(attrs={"class": "next_page"})
 # print(content.find_all(attrs={"class": "next_page"}))
 def fill_books():
+    time.sleep(2)
     # the table of books for this page
     table_data = content.find_all("tr")
+    count = 0
     for item in table_data:
         book = {}
+
+        # Title
         book_title = item.find("a", class_= "bookTitle")
         # Had to chop off the ends of the title to remove \n values
         book["title"] = book_title.get_text()[1:-1]
+
+        # Author
         book_author = item.find("a", class_="authorName")
         book["author"] = book_author.get_text()
+
+        # Detailed info that requires going to book page
+        book_link = website + item.find("a", class_="bookTitle", href=True)['href']
+        temp_response = requests.get(book_link, timeout=10)
+        temp_content = BeautifulSoup(temp_response.content, "html.parser")
+
+        # Summary
+        span_tags = temp_content.find("div", class_="readable stacked", id="description").findChildren("span")
+        book_summary = ""
+        if (len(span_tags)>1):
+            book_summary = span_tags[1].get_text()
+        elif(len(span_tags) == 1):
+            book_summary = span_tags.get_text()
+        book["book summary"] = book_summary
+
+        # Characters
+        detail_tags = temp_content.find("div", class_="uitext", id="bookDataBox").findChildren("div")
+        flag = False
+        book_characters = ""
+        for detail in detail_tags:
+            if (flag):
+                b_characters = detail.find_all("a")
+                flag = False
+            # print(detail.get_text())
+            if (detail.get_text() == "Characters"):
+                flag = True
+        book_characters = []
+        for character in range(len(b_characters)-1):
+            b_characters[character] = b_characters[character].get_text()
+            if (b_characters[character] != '...more' and b_characters[character] != '...less'):
+                book_characters.append(b_characters[character])
+        book["characters"] = book_characters
         books.append(book)
 
 while (len(content.find_all(attrs={"class": "next_page disabled"})) == 0):
     fill_books()
     pages = content.find_all(attrs={"class": "next_page"})
     link = pages[0]['href']
-    print(url)
     url = website + link
     response = requests.get(url, timeout=10)
     content = BeautifulSoup(response.content, "html.parser")
 
 # Fill books one more time for the last page
 fill_books()
+print(books)
 print(len(books))
 
 with open("goodreads.csv", 'w') as myfile:
